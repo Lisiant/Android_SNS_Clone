44551f9d69579288eda043adba0a01ce93f62907
Jangwoo <wkdxod321@gmail.com>
2023-01-07 00:44:31 +0900

    viewBinding, BottomNavigationView 적용

---
+++ DailyQ/app/src/main/java/online/dailyq/ui/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package online.dailyq.ui.base
+
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+}
---
+++ DailyQ/app/src/main/java/online/dailyq/ui/main/MainActivity.kt
@@ -0,0 +1,47 @@
+package online.dailyq.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import online.dailyq.R
+import online.dailyq.databinding.ActivityMainBinding
+import online.dailyq.ui.base.BaseActivity
+import online.dailyq.ui.profile.ProfileFragment
+import online.dailyq.ui.timeline.TimelineFragment
+import online.dailyq.ui.today.TodayFragment
+
+class MainActivity : BaseActivity() {
+
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navView.setOnItemSelectedListener {
+            val ft = supportFragmentManager.beginTransaction()
+
+            when (it.itemId) {
+                R.id.timeline -> {
+                    ft.replace(R.id.host, TimelineFragment())
+                    supportActionBar?.setTitle(R.string.title_timeline)
+                }
+                R.id.today -> {
+                    ft.replace(R.id.host, TodayFragment())
+                    supportActionBar?.setTitle(R.string.title_timeline)
+                }
+                R.id.profile -> {
+                    ft.replace(R.id.host, ProfileFragment())
+                    supportActionBar?.setTitle(R.string.title_timeline)
+                }
+            }
+            ft.commit()
+            true
+        }
+
+        binding.navView.selectedItemId = R.id.today
+
+    }
+}
+
