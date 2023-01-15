ecb9d7a83088d3fd997c57f45a7662841920b2eb
Jangwoo <wkdxod321@gmail.com>
2023-01-15 19:40:52 +0900

    add: fix TodayFragment, fix BaseActivity, BaseFragment ApiService 싱글톤 패턴 적용

--- DailyQ/app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
+++ DailyQ/app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
@@ -4,23 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.gson.FieldNamingPolicy
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import online.dailyq.api.response.HelloWorld
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import online.dailyq.api.ApiService
 import online.dailyq.databinding.FragmentTodayBinding
 import online.dailyq.ui.base.BaseFragment
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
 import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 class TodayFragment : BaseFragment() {
     var _binding: FragmentTodayBinding? = null
     val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,44 +32,26 @@ class TodayFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Thread {
-            val url = URL("http://10.0.2.2:5000/v1/hello-world")
-            val conn = url.openConnection() as HttpURLConnection
-            conn.connectTimeout = 5000
-            conn.readTimeout = 5000
-            conn.requestMethod = "GET"
-            conn.setRequestProperty("Accept", "application/json")
-            conn.connect()
 
-            val reader = BufferedReader(InputStreamReader(conn.inputStream))
-            val body = reader.readText()
-            reader.close()
-            conn.disconnect()
+        viewLifecycleOwner.lifecycleScope.launch {
 
-//            val gson = Gson()
-
-            val gson = GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .create()
+            val qidDateFormat = SimpleDateFormat("yyyy-MM-dd")
+            val qid = qidDateFormat.format(Date())
+            val question = api.getQuestion(qid)
 
             val dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.KOREA)
-            val helloWorld = gson.fromJson(body, HelloWorld::class.java)
+            binding.date.text = dateFormat.format(qidDateFormat.parse(question.id))
+            binding.question.text = question.text
 
-//            val json = JSONObject(body)
-//            val date = json.getString("date").substring(0,10)
-//            val message = json.getString("message")
-            activity?.runOnUiThread {
-                binding.question.text = helloWorld.message
-                binding.date.text = dateFormat.format(helloWorld.date)
-            }
-        }.start()
+        }
+
+
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
-
 
 
 }
