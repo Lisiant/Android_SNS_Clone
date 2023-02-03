e11b7eb6be3abf91e8494ec8a1d6d200dbdda36d
Lisiant <wkdxod321@gmail.com>
2023-02-03 21:07:25 +0900

    7.4 WriteActivity Multipart 사용 완료

--- DailyQ/app/src/main/java/online/dailyq/api/Extensions.kt
+++ DailyQ/app/src/main/java/online/dailyq/api/Extensions.kt
@@ -8,6 +8,11 @@ import okhttp3.RequestBody
 import okio.BufferedSink
 import okio.source
 
+/**
+ * Content Resolver 로 MultipartBody.Part를 생성할 때 필요한(value 값으로) RequestBody를 리턴
+ *
+ * writeTo의 sink에 데이터를 넣어주면 이미지를 넣을 수 있다.
+ */
 fun Uri.asRequestBody(cr: ContentResolver): RequestBody {
     return object : RequestBody() {
         override fun contentType(): MediaType? = cr.getType(this@asRequestBody)?.toMediaTypeOrNull()
--- DailyQ/app/src/main/java/online/dailyq/ui/write/WriteActivity.kt
+++ DailyQ/app/src/main/java/online/dailyq/ui/write/WriteActivity.kt
@@ -1,12 +1,17 @@
 package online.dailyq.ui.write
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.launch
 import okhttp3.MultipartBody
 import online.dailyq.R
@@ -20,7 +25,8 @@ import java.time.format.DateTimeFormatter
 
 /**
  * 답을 작성하거나 수정하는 액티비티
- * 질문: WriteActvity를 시작하는 곳에서 intent를 통해서 전달받는다.
+ *
+ * Question: WriteActvity를 시작하는 곳에서 intent를 통해서 전달받는다.
  */
 class WriteActivity : BaseActivity() {
     companion object {
@@ -36,20 +42,37 @@ class WriteActivity : BaseActivity() {
     lateinit var mode: Mode
     lateinit var question: Question
     var answer: Answer? = null
+
+    // 이미지 API에서 받아온 Url을 담는 변수
     var imageUrl: String? = null
 
+    // 이후 onOptionsItemSelected에서 이미지를 받아오는 액션을 수행할 때 사용
     val startForResult =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-            if (result.resultCode == Activity.RESULT_OK){
-                lifecycleScope.launch{
-                    val imageUrl = result.data?.data ?: return@launch
-                    val requestBody = imageUrl.asRequestBody(contentResolver)
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                lifecycleScope.launch {
+                    // Url이 아니라 Uri임에 주의
+                    val imageUri = result.data?.data ?: return@launch
 
-                    // createFormdata
-                    val part = MultipartBody.Part.createFormData("image", )
+                    // Extensions.kt 에서 작성한 확장 함수 사용
+                    val requestBody = imageUri.asRequestBody(contentResolver)
+
+                    // MultipartBody.Part 생성 후 이미지 업로드 수행하는 API 호출
+                    val part = MultipartBody.Part.createFormData("image", "filename", requestBody)
+                    val imageResponse = api.uploadImage(part)
+
+                    // API 호출 성공적인 경우 (이미지 업로드에 성공한 경우) 하단 썸네일에 표시
+                    if (imageResponse.isSuccessful) {
+                        imageUrl = imageResponse.body()!!.url
+
+                        // Coil 사용해서 imageView 에서 바로 확장함수 load 사용 가능
+                        binding.photo.load(imageUrl) {
+                            transformations(RoundedCornersTransformation(resources.getDimension(R.dimen.thumbnail_rounded_corner)))
+                        }
+                        binding.photoArea.isVisible = true
+                    }
                 }
             }
-
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,9 +92,40 @@ class WriteActivity : BaseActivity() {
         lifecycleScope.launch {
             question = api.getQuestion(qid).body()!!
             answer = api.getAnswer(qid).body()
+
             binding.question.text = question.text
             binding.answer.setText(answer?.text)
+
+            imageUrl = answer?.photo
+            binding.photoArea.isVisible = !imageUrl.isNullOrEmpty()
+
+            // let scope function 으로 load 확장함수의 매개변수로 imageUrl 전달.
+            imageUrl?.let {
+                binding.photo.load(it) {
+                    transformations(RoundedCornersTransformation(resources.getDimension(R.dimen.thumbnail_rounded_corner)))
+                }
+            }
+
         }
+
+        binding.photoArea.setOnClickListener {
+            showDeleteConfirmDialog()
+        }
+
+    }
+
+    fun showDeleteConfirmDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setMessage(R.string.dialog_msg_are_you_sure_to_delete)
+            .setPositiveButton(android.R.string.ok) { dialog, which ->
+                binding.photo.setImageResource(0)
+                binding.photoArea.isVisible = false
+                imageUrl = null
+            }
+            .setNegativeButton(android.R.string.cancel) { dialog, which ->
+
+            }.show()
+
     }
 
     // 앱바에 메뉴 설정하기
@@ -80,11 +134,22 @@ class WriteActivity : BaseActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    // 완료 버튼을 눌렀을 때 write() 실행
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
+            // 완료 버튼을 눌렀을 때 write() 실행
             R.id.done -> {
                 write()
+                return true
+            }
+            // 사진 추가 버튼을 눌렀을 때 사진 선택하기 화면으로 전환
+            R.id.add_photo -> {
+
+                startForResult.launch(
+                    Intent(Intent.ACTION_GET_CONTENT).apply {
+                        addCategory(Intent.CATEGORY_OPENABLE)
+                        type = "image/*"
+                        putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png"))
+                    })
                 return true
             }
         }
@@ -96,9 +161,9 @@ class WriteActivity : BaseActivity() {
         val text = binding.answer.text.toString().trimEnd()
         lifecycleScope.launch {
             val answerResponse = if (answer == null) {
-                api.writeAnswer(question.id, text)
+                api.writeAnswer(question.id, text, imageUrl)
             } else {
-                api.editAnswer(question.id, text)
+                api.editAnswer(question.id, text, imageUrl)
             }
 
             if (answerResponse.isSuccessful) {
