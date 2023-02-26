18f83dec2f6b201e006295436e791a8d501f0386
Lisiant <wkdxod321@gmail.com>
2023-02-26 16:53:45 +0900

    8.3.1 add: TimelineLoadState~ 무한 페이지 로딩 기능 추가 책 페이지 245까지 완료

--- DailyQ/app/src/main/java/online/dailyq/ui/timeline/TimelineFragment.kt
+++ DailyQ/app/src/main/java/online/dailyq/ui/timeline/TimelineFragment.kt
@@ -1,60 +1,53 @@
 package online.dailyq.ui.timeline
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import online.dailyq.R
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import online.dailyq.databinding.FragmentTimelineBinding
+import online.dailyq.ui.base.BaseFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class TimelineFragment : BaseFragment() {
+    var _binding: FragmentTimelineBinding? = null
+    val binding get() = _binding!!
+    lateinit var adapter: TimelineAdapter
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TimelineFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TimelineFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTimelineBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            adapter = TimelineAdapter(requireContext())
+            recycler.adapter =
+                adapter.withLoadStateFooter(TimelineLoadStateAdapter { adapter.retry() })
+            recycler.layoutManager = LinearLayoutManager(context)
+        }
+
+        lifecycleScope.launch {
+            Pager(PagingConfig(initialLoadSize = 6, pageSize = 3, enablePlaceholders = false)) {
+                TimelinePagingSource(api)
+            }.flow.collectLatest {
+                adapter.submitData(it)
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timeline, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TimelineFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TimelineFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
